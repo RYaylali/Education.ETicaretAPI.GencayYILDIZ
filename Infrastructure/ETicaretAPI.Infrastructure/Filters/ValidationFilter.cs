8b6d3e9aa451bfcaaee6276f0abfbe8d420435d0
Ramazan YAYLALI <ryaylali97@gmail.com>
2023-03-14 23:23:43 +0300

    Flent Validations

---
+++ Infrastructure/ETicaretAPI.Infrastructure/Filters/ValidationFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Infrastructure.Filters
+{
+    public class ValidationFilter : IAsyncActionFilter//action a gelen isteklerde devreye girer
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)//gelen isteklerin hatalarını bulma
+            {
+                var errors = context.ModelState
+                     .Where(x => x.Value.Errors.Any())
+                     .ToDictionary(e => e.Key, e => e.Value.Errors.Select(e => e.ErrorMessage)).ToArray();//key le gelen eror numarasına karşılık gelen eror mesajlarını getirir
+                context.Result = new BadRequestObjectResult(errors);
+            }
+            await next();
+        }
+    }
+}
--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -1,4 +1,7 @@
+using ETicaretAPI.Application.Validators.Products;
+using ETicaretAPI.Infrastructure.Filters;
 using ETicaretAPÝ.Persistence;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +11,9 @@ builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
 policy.WithOrigins("http://localhost:4200", "https://localhost:4200/").AllowAnyHeader().AllowAnyMethod()//bütün header ve methodlara cevap verir.
 //bu yerden gelen istekleri al 
 ));
-builder.Services.AddControllers();
+builder.Services.AddControllers(options=>options.Filters.Add<ValidationFilter>())//infrastructure de oluþturduðumuz filtremeleme mesaþlarýna ulaþmasý için
+    .AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())//Burada validasyonlarý kontrol edeceði yeri göstermek için createproductvalidators class verildi fluenvalidation için
+    .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);//mevcut olan doðrulama filtrelerini bastýr. api tetiklenecek
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
